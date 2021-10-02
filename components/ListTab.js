953f4c1ee5afcc6697fbadd4f5b5bf4c236f8ba2
gplaz005 <gplaz005@fiu.edu>
2021-10-02 18:59:07 -0400

    formik validation

--- components/ListTab.js
+++ components/ListTab.js
@@ -1,27 +1,65 @@
-import React from 'react'
-import { View, Text, StyleSheet , Button} from 'react-native'
+import React  ,{useState}from 'react'
+import { View, Text, StyleSheet , Button, TextInput} from 'react-native'
+import {Formik} from 'formik'; 
+import * as Yup from 'yup';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const ListTab = ({navigation}) => {
+
+    const [text, onChangeText] = useState('');
+
     
     return(
         <View style ={styles.container}>
+       
+       <Formik
+     initialValues={{ email: '', password: '' }}
+     onSubmit={values => console.log(values)}
+    validationSchema = {
+                Yup.object({
+                    email: Yup.string()
+                    .trim()
+                    .email('invalid email')
+                    .required('please enter email'),
+                    password: Yup.string()
+                    .required('please enter password')
+                })
+            }
+
+
+   >
+     {({ handleChange, errors , touched, handleBlur, handleSubmit, values }) => (
+       <View>
+           {errors.email && touched.email? <Text>invalid email format</Text> : null}
+            <View style ={styles.input}>
+            <TextInput
+           onChangeText={handleChange('email')}
+           onBlur={handleBlur('email')}
+           value={values.email}
+         />
+         </View>
+
+         
+         {errors.password && touched.password? <Text>invalid password format</Text> : null}
+         <View style ={styles.input}>
+            <TextInput
+            secureTextEntry = {true}
+           onChangeText={handleChange('password')}
+           onBlur={handleBlur('password')}
+           value={values.password}
+         />
+         </View>
+         <Button onPress={handleSubmit} title="Submit" />
+       </View>
+     )}
+   </Formik>
+            
+
+        
         <Button
         title="Names Tab"
         onPress={() => navigation.navigate('NamesTab')}
         />
-
-        <Button
-        title="Image Tab"
-        onPress={() => navigation.navigate('ImageTab')}
-        />
-
-        <Button
-        title="Video Tab"
-        onPress={() => navigation.navigate('VideoTab')}
-        />
-
-        
-
         
         </View>
     )
@@ -35,5 +73,12 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
-    }
+    },
+    input: {
+        height: 40,
+        width: 200,
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 40
+      },
 })
